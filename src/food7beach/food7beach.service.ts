d5c9a754039c4b38be45ec2b12f49769333cf08c
장경찬 <zerone@jang-gyeongchan-ui-Macmini.local>
2023-05-11 17:53:09 +0900

    feat: api 형태

--- src/food7beach/food7beach.service.ts
+++ src/food7beach/food7beach.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 
 @Injectable()
-export class Food7beachService {}
+export class Food7beachService {
+  async getFood7beach() {
+    try {
+      const response = await axios.get(
+        'https://busan-7beach.openapi.redtable.global/api/rstr?serviceKey= k6n6auirB4RainsimXQsPzYW1KXz2i3xBgaqKX54lXGcENz2sHTg7g4zuLG3BioL',
+      );
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
