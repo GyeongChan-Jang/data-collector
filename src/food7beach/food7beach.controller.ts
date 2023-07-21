a5487f9f47dbeeec98ca0cc6b6fb84ed8bb08e57
chan <cham9994@gmail.com>
2023-07-21 22:14:54 +0900

    update

--- src/food7beach/food7beach.controller.ts
+++ src/food7beach/food7beach.controller.ts
@@ -8,37 +8,37 @@ export class Food7beachController {
   constructor(private food7beachService: Food7beachService) {}
 
   // @Cron(CronExpression.EVERY_10_SECONDS)
-  @Get('rstr')
+  @Get('rstr') // 1
   async getRstr() {
     return await this.food7beachService.getRstr();
   }
 
-  @Get('menu/korean')
+  @Get('menu/korean') // 2
   async getMenu() {
     return await this.food7beachService.getMenu();
   }
 
-  @Get('menu-dscrn/korean')
+  @Get('menu-dscrn/korean') // 4
   async getMenuDetail() {
     return await this.food7beachService.getMenuDetail();
   }
 
-  @Get('rstr/img')
+  @Get('rstr/img') // 3
   async getRstrImage() {
     return await this.food7beachService.getRstrImage();
   }
 
-  @Get('food/img')
+  @Get('food/img') // 5
   async getFoodImage() {
     return await this.food7beachService.getFoodImage();
   }
 
-  @Get('rstr/oprt')
+  @Get('rstr/oprt') // 6
   async getRstrOperationgInfo() {
     return await this.food7beachService.getRstrOperatingInfo();
   }
 
-  @Get('rstr/qlt')
+  @Get('rstr/qlt') // 7
   async getRstrQuality() {
     return await this.food7beachService.getRstrQualityInfo();
   }
